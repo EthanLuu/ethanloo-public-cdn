fb9f306b777f9c13748fdeed8b524b309c0859df
ethanol <952792901@qq.com>
2020-12-01 14:46:03 +0800

    gitcalendar

--- gitcalendar.js
+++ gitcalendar.js
@@ -244,7 +244,7 @@ $(function () {
       window.onscroll = function () {
         $(".angle-wrapper").hide();
       };
-      console.log(calendar.positionplusdata);
+      // console.log(calendar.positionplusdata);
 
       function addlastmonth() {
         if (calendar.thisdayindex === 0) {
