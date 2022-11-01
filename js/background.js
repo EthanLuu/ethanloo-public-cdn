7885bffe41ebb9cd02285f72ac2b2e28547f95e0
EthanLuu <ethanlooo@outlook.com>
2022-11-01 17:31:59 +0800

    ✨ feat: add new header for blog

--- js/background.js
+++ js/background.js
@@ -2,3 +2,5 @@ var full_page = document.getElementsByClassName("full_page");
 if (full_page.length != 0) {
   full_page[0].style.background = "transparent";
 }
+
+"" === GLOBAL_CONFIG_SITE.title.replace("EthanLoo", "") ? document.getElementById("page-name-text").style.display = "none" : document.querySelector("#page-name-text>span").innerHTML = document.title.split(" | EthanLoo")[0];
